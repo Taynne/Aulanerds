226cfd19aa6417395115577f736e7af51be52ddd
Taynne <taynnekelly@hotmail.com>
2014-08-08 11:19:55 -0400

    Update inserir_aluno.php

--- inserir_aluno.php
+++ inserir_aluno.php
@@ -5,8 +5,8 @@ $idade_aluno = $_POST["idade_aluno"];
 include 'conexao.php';
 
 $sql = "insert into pessoa(nome_aluno,materia)values ('$nome_aluno','$nome_materia');";
-mysql_query($sql);
-mysql_close($conexao);
+mysql_query($sql); #abre conexao
+mysql_close($conexao); #Fecha conexao
 echo "<script>document.location = 'index.php'</script>";
 
 
